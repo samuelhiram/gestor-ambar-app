a586b8b0faf9896ad43696276a3cb67ff37d927a
gitelam <smiotics@gmail.com>
2024-09-14 22:04:52 -0700

    fixed sidebar sm- behavior at sm screen size

--- src/app/main/components/SidebarMenuItem.jsx
+++ src/app/main/components/SidebarMenuItem.jsx
@@ -6,7 +6,6 @@ import { useMainAppContext } from "./MainAppContext";
 export default function SidebarMenuItem({ receivedModule }) {
   const { state, setState } = useMainAppContext();
   const { icon, title, description, moduleName } = receivedModule;
-  console.log(`ocurrs in ${moduleName}: `, state.activeModuleName, moduleName);
   return (
     <>
       <div
@@ -18,8 +17,43 @@ export default function SidebarMenuItem({ receivedModule }) {
           });
         }}
         className={`${
-          state.activeModuleName === moduleName ? "bg-blue-200/50" : ""
-        } w-full flex justify-between bg-gray-50  hover:bg-gray-100 cursor-pointer active:bg-gray-200/80 p-1`}
+          state.activeModuleName === moduleName ? "bg-blue-200" : ""
+        } sm:hidden w-full flex justify-between bg-gray-50  hover:bg-gray-100 cursor-pointer active:bg-gray-200/80 p-1`}
+      >
+        <div className="flex items-center justify-start gap-2 w-4/5">
+          <div>
+            <Icon
+              icon={icon}
+              width="38"
+              height="38"
+              style={{ color: "#1e3a8a" }}
+            />
+          </div>
+          <div className="text-md">
+            <p>{title}</p>
+            <p className="text-xs">{description}</p>
+          </div>
+        </div>
+        <div className=" flex items-center justify-center w-1/5">
+          <Icon
+            icon="fluent:ios-arrow-right-24-regular"
+            width="24"
+            height="24"
+            style={{ color: "#1e3a8a" }}
+          />
+        </div>
+      </div>
+      <div
+        onClick={() => {
+          setState({
+            ...state,
+            activeModuleName: moduleName,
+            // showSideBar: false,
+          });
+        }}
+        className={`${
+          state.activeModuleName === moduleName ? "bg-blue-200" : ""
+        } max-sm:hidden w-full flex justify-between bg-gray-50  hover:bg-gray-100 cursor-pointer active:bg-gray-200/80 p-1`}
       >
         <div className="flex items-center justify-start gap-2 w-4/5">
           <div>
