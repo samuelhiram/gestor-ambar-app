2d14e7d675734f314a5594b59136e384a1fd04b9
gitelam <smiotics@gmail.com>
2024-11-12 15:51:50 -0800

    added inputs to forms Caution --*

--- src/app/main/components/modules/components/CreateCategory.jsx
+++ src/app/main/components/modules/components/CreateCategory.jsx
@@ -187,7 +187,9 @@ export default function CreateCategory() {
                   d="M6.5 11L12 2l5.5 9zm11 11q-1.875 0-3.187-1.312T13 17.5t1.313-3.187T17.5 13t3.188 1.313T22 17.5t-1.312 3.188T17.5 22M3 21.5v-8h8v8z"
                 />
               </svg>
-              <h1 className="text-sm font-bold">Crear categoria</h1>
+              <h1 className="text-sm font-bold">
+                Crear categoria / núm. partida
+              </h1>
             </div>
           </AccordionTrigger>
           <AccordionContent className="p-2">
--- src/app/main/components/modules/components/CreateItem.jsx
+++ src/app/main/components/modules/components/CreateItem.jsx
@@ -39,158 +39,50 @@ import { Input } from "@/components/ui/input";
 import { useMainAppContext } from "../../MainAppContext";
 
 /////////////////
-/////////////////
-/////////////////
 export default function CreateItem() {
-  const [locations, setLocations] = useState([]);
   const [isSubmitting, setIsSubmitting] = useState(false);
   const { state, setState } = useMainAppContext();
   const { toast } = useToast();
   const formSchema = z.object({
-    email: z
-      .string()
-      .email({ message: "Formato inválido" })
-      .min(2, { message: "Obligatorio" })
-      .max(50),
-    name: z.string().min(2, { message: "Obligatorio" }).max(50),
-    control_number: z.string().min(6, { message: "Obligatorio" }).max(50),
-    location: z.string().min(2, { message: "Obligatorio" }).max(50),
-    //make with zod required
-    role: z.string().min(2, { message: "Obligatorio" }).max(50),
+    barCode: z.string(),
+    name: z.string().min(1),
+    description: z.string().min(1),
+    quantity: z
+      .number()
+      .int()
+      .min(0, { message: "La cantidad debe ser un número entero positivo" }),
+    unitId: z.string().min(1),
+    typeId: z.string(),
+    categoryId: z.string().min(1),
+    locationId: z.string().min(1),
+    userId: z.string().min(1),
+    ubicationId: z.string().min(1),
   });
 
   // 1. Define your form.
   const form = useForm({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      email: "",
+      barCode: "",
       name: "",
-      control_number: "",
-      role: "",
-      location: "",
+      description: "",
+      quantity: "",
+      unitId: "",
+      typeId: "",
+      categoryId: "",
+      locationId: "",
+      userId: "",
+      ubicationId: "",
     },
   });
-  const sendPasswordEmail = async (email, password) => {
-    // Inicializar EmailJS con tu clave pública
-    emailjs.init({
-      publicKey: "vGvH9vq_aIaVCEh1D",
-    }); // Asegúrate de reemplazar "TU_CLAVE_PUBLICA" por tu clave real
-
-    const templateParams = {
-      email: email,
-      password: password,
-    };
-
-    try {
-      // console.log("Enviando email con los parámetros:", templateParams);
-      // Enviar el correo usando async/await
-      await emailjs.send("asiscan", "template_mei0db8", templateParams);
-      // console.log("Correo enviado correctamente ----- SUCCESS!");
-    } catch (error) {
-      console.error("Error al enviar el correo ----- FAILED...", error);
-    }
-  };
 
   async function onSubmit(values) {
     setIsSubmitting(true);
-    const password = Math.random().toString(36).slice(-8);
 
-    values.password = password;
-
-    console.log(values);
-
-    try {
-      // Ocultar alerta de diálogo al iniciar el proceso
-      setState((prev) => ({
-        ...prev,
-        showDialogAlert: false,
-      }));
-
-      const response = await fetch("/api/users/create", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${state.token}`, // Asegúrate de que 'token' esté definido correctamente
-        },
-        body: JSON.stringify(values), // 'values' es el objeto que contiene los datos del formulario
-      });
-
-      // Verificar si la respuesta no fue exitosa
-      if (!response.ok) {
-        const error = await response.json(); // Obtener el mensaje de error del servidor
-
-        setState((prevState) => ({
-          ...prevState,
-          showDialogAlert: true,
-          dialogMessage:
-            error.error === "Email already exists"
-              ? "El correo ya está registrado"
-              : "El número de control ya está registrado",
-        }));
-        setIsSubmitting(false);
-
-        return; // Salir si ocurre un error
-      }
-
-      // Obtener datos del usuario si la solicitud fue exitosa
-      const data = await response.json();
-      console.log("User registered successfully:", data); // Muestra los datos devueltos por el backend
-
-      sendPasswordEmail(values.email, password);
-
-      toast({
-        className:
-          "top-0 right-0 flex fixed md:max-w-[420px] md:top-2 md:right-2",
-        position: "top-right",
-        variant: "success",
-        title: "Usuario creado...",
-      });
-
-      // Obtener la lista de usuarios actualizada
-      const usersResponse = await fetch("/api/users/get", {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${state.token}`, // Asegúrate de que 'token' esté definido correctamente
-        },
-      });
-
-      if (!usersResponse.ok) {
-        throw new Error("Failed to fetch users");
-      }
-
-      const usersData = await usersResponse.json();
-
-      // Actualizar el estado con los datos de los usuarios
-      setState((prevState) => ({
-        ...prevState,
-        usersData: usersData.users,
-      }));
-
-      // Limpiar el formulario
-      form.reset({
-        email: "",
-        fullName: "",
-        control_number: "",
-        role: "",
-        location: "",
-      });
-
-      setIsSubmitting(false);
-    } catch (err) {
-      console.error("Error en la petición:", err.message);
-      toast({
-        className:
-          "top-0 right-0 flex fixed md:max-w-[420px] md:top-2 md:right-2",
-        position: "top-right",
-        variant: "destructive",
-        title: "Error al crear usuario...",
-      });
-      setIsSubmitting(false);
-    }
+    setIsSubmitting(false);
   }
   return (
-    <div className="w-full p-2 border rounded-xl">
+    <div className="w-full p-2 border  rounded-xl">
       <Accordion
         className="w-full !border-0 !border-b-0 "
         type="single"
@@ -220,59 +112,80 @@ export default function CreateItem() {
           <AccordionContent className="p-2">
             <Form {...form}>
               <form
+                className="flex flex-col gap-2"
                 onSubmit={form.handleSubmit(onSubmit)}
-                className="w-full flex gap-2 items-end max-xl:flex-col"
               >
+                <div className="w-full flex gap-2 items-end max-xl:flex-col">
+                  <FormField
+                    control={form.control}
+                    name="name"
+                    render={({ field }) => (
+                      <FormItem className="w-full flex-grow">
+                        <FormLabel>*Nombre</FormLabel>
+                        <FormControl>
+                          <Input
+                            type="text"
+                            placeholder="Nombre del suministro..."
+                            {...field}
+                          />
+                        </FormControl>
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="description"
+                    render={({ field }) => (
+                      <FormItem className="w-full flex-grow">
+                        <FormLabel>*Descripción</FormLabel>
+                        <FormControl>
+                          <Input
+                            type="email"
+                            placeholder="Descripción del suministro..."
+                            {...field}
+                          />
+                        </FormControl>
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="typeId"
+                    render={({ field }) => (
+                      <FormItem className="w-[400px] flex-grow">
+                        <FormLabel>Tipo</FormLabel>
+                        <FormControl>
+                          <Select
+                            onValueChange={(value) => {
+                              field.onChange(value); // Actualiza el valor en el estado del formulario
+                            }}
+                            value={field.value || ""} // Asegúrate de que el valor sea un string vacío si no hay valor
+                          >
+                            <SelectTrigger className="!bg-white !rounded-md !text-gray-600">
+                              <SelectValue placeholder="Seleccione tipo..." />
+                            </SelectTrigger>
+                            <SelectContent className="!m-0 !p-0 !w-auto">
+                              <SelectGroup>
+                                <SelectLabel>Tipo</SelectLabel>
+                                <SelectItem value="insumo">Insumo</SelectItem>
+                                <SelectItem value="herramienta">
+                                  Herramienta
+                                </SelectItem>
+                              </SelectGroup>
+                            </SelectContent>
+                          </Select>
+                        </FormControl>
+                      </FormItem>
+                    )}
+                  />
+                </div>
+
                 <FormField
                   control={form.control}
-                  name="name"
+                  name="categoryId"
                   render={({ field }) => (
                     <FormItem className="w-full flex-grow">
-                      <FormLabel>Nombre</FormLabel>
-                      <FormControl>
-                        <Input
-                          type="text"
-                          placeholder="Ana Gutierrez"
-                          {...field}
-                        />
-                      </FormControl>
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="email"
-                  render={({ field }) => (
-                    <FormItem className="w-full flex-grow">
-                      <FormLabel>Correo institucional</FormLabel>
-                      <FormControl>
-                        <Input
-                          type="email"
-                          placeholder="@tectijuana.edu.mx"
-                          {...field}
-                        />
-                      </FormControl>
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="control_number"
-                  render={({ field }) => (
-                    <FormItem className="w-full flex-grow">
-                      <FormLabel>Número de control</FormLabel>
-                      <FormControl>
-                        <Input type="text" placeholder="20212903" {...field} />
-                      </FormControl>
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="role"
-                  render={({ field }) => (
-                    <FormItem className="w-full flex-grow">
-                      <FormLabel>Rol</FormLabel>
+                      <FormLabel>*Categoría</FormLabel>
                       <FormControl>
                         <Select
                           onValueChange={(value) => {
@@ -281,14 +194,12 @@ export default function CreateItem() {
                           value={field.value || ""} // Asegúrate de que el valor sea un string vacío si no hay valor
                         >
                           <SelectTrigger className="!bg-white !rounded-md !text-gray-600">
-                            <SelectValue placeholder="Seleccione Rol" />
+                            <SelectValue placeholder="Seleccione tipo..." />
                           </SelectTrigger>
                           <SelectContent className="!m-0 !p-0 !w-auto">
                             <SelectGroup>
-                              <SelectLabel>Rol</SelectLabel>
-                              <SelectItem value="admin">Admin</SelectItem>
-                              <SelectItem value="viewer">Viewer</SelectItem>
-                              <SelectItem value="mod">Mod</SelectItem>
+                              <SelectLabel>Categoría</SelectLabel>
+                              <SelectItem value="insumo">etc...</SelectItem>
                             </SelectGroup>
                           </SelectContent>
                         </Select>
@@ -296,46 +207,54 @@ export default function CreateItem() {
                     </FormItem>
                   )}
                 />
-                <FormField
-                  control={form.control}
-                  name="location"
-                  render={({ field }) => (
-                    <FormItem className="w-full flex-grow">
-                      <FormLabel>Lugar</FormLabel>
-                      <FormControl>
-                        <Select
-                          onValueChange={(value) => {
-                            field.onChange(value); // Actualiza el valor en el estado del formulario
-                          }}
-                          value={field.value || ""} // Asegúrate de que el valor sea un string vacío si no hay valor
-                        >
-                          <SelectTrigger className="w-full !bg-white !rounded-md !text-gray-600">
-                            <SelectValue placeholder="Seleccione plantel" />
-                          </SelectTrigger>
-                          <SelectContent className="!m-0 !p-0 !w-auto">
-                            <SelectGroup>
-                              <SelectLabel>Unidad</SelectLabel>
-                              {/* <SelectItem value="Unidad Tomas Aquino">
-                                Unidad Tomas Aquino
-                              </SelectItem>
-                              <SelectItem value="Unidad Otay">
-                                Unidad Otay
-                              </SelectItem> */}
-                              {state.locations.map((location) => (
-                                <SelectItem
-                                  key={location.id}
-                                  value={location.id}
-                                >
-                                  {location.name}
-                                </SelectItem>
-                              ))}
-                            </SelectGroup>
-                          </SelectContent>
-                        </Select>
-                      </FormControl>
-                    </FormItem>
-                  )}
-                />
+
+                <div className="flex gap-2">
+                  <FormField
+                    control={form.control}
+                    name="quantity"
+                    render={({ field }) => (
+                      <FormItem className="w-full flex-grow">
+                        <FormLabel>*Cantidad</FormLabel>
+                        <FormControl>
+                          <Input
+                            type="email"
+                            placeholder="Descripción del suministro..."
+                            {...field}
+                          />
+                        </FormControl>
+                      </FormItem>
+                    )}
+                  />
+
+                  <FormField
+                    control={form.control}
+                    name="unitId"
+                    render={({ field }) => (
+                      <FormItem className="w-full flex-grow">
+                        <FormLabel>*Unidad</FormLabel>
+                        <FormControl>
+                          <Select
+                            onValueChange={(value) => {
+                              field.onChange(value); // Actualiza el valor en el estado del formulario
+                            }}
+                            value={field.value || ""} // Asegúrate de que el valor sea un string vacío si no hay valor
+                          >
+                            <SelectTrigger className="!bg-white !rounded-md !text-gray-600">
+                              <SelectValue placeholder="Seleccione tipo..." />
+                            </SelectTrigger>
+                            <SelectContent className="!m-0 !p-0 !w-auto">
+                              <SelectGroup>
+                                <SelectLabel>Unidad</SelectLabel>
+                                <SelectItem value="insumo">etc...</SelectItem>
+                              </SelectGroup>
+                            </SelectContent>
+                          </Select>
+                        </FormControl>
+                      </FormItem>
+                    )}
+                  />
+                </div>
+
                 <Button disabled={isSubmitting} type="submit">
                   Crear
                 </Button>
